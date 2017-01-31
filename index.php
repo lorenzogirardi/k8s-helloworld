74bdb5842977378d33488e6d734997e6b925de48
lorenzogirardi <lorenzo@ethzero.it>
2017-01-31 20:21:49 +0100

    autorefresh 3 sec

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<html>
+    <head>
+   <meta http-equiv="refresh" content="3" >  
+   </head>
 <?php
-  echo "Hello k8s World!<p>";
+  echo "Hello World! k8s edition<p>";
   echo "k8s Pod IP Address Is: " . $_SERVER['SERVER_ADDR'] . "<br>";
-?>
-
+?> 
+<body>
+</html>
