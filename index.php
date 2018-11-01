211162f6031003de54c5d5bddd3670f20b1a8c26
lgirardi <lorenzo.girardi@lastminute.com>
2018-11-01 19:44:35 +0100

    disable warning

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 ?></h1>
 
 <?php
+  ini_set( "display_errors", 0);  
   header("Cache-Control: no-cache, must-revalidate");
 	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 	header("Pragma: no-cache");
